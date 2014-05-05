a628320ac973ab0ab4c778ff38018861563f42ff
Jakob Gruber <jakob.gruber@gmail.com>
2014-05-05 10:53:56 +0200

    Initial item class version

---
+++ src/clsm/item.h
@@ -0,0 +1,46 @@
+/*
+ *  Copyright 2014 Jakob Gruber
+ *
+ *  This file is part of kpqueue.
+ *
+ *  kpqueue is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  kpqueue is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with kpqueue.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __ITEM_H
+#define __ITEM_H
+
+#include <atomic>
+#include <thread>
+
+namespace kpq
+{
+
+typedef uint32_t version_t;
+
+template <class K, class V>
+class item
+{
+public:
+    item();
+
+private:
+    std::atomic<version_t> m_version;
+    K m_key;
+    V m_val;
+    std::thread::id m_owner;
+};
+
+}
+
+#endif /* __ITEM_H */
