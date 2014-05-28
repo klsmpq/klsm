cd9988e6426a734f08ed920c7c0c634b18dd472f
Jakob Gruber <jakob.gruber@gmail.com>
2014-05-28 10:51:22 +0200

    Initial shortest paths bench implementation

--- src/bench/shortest_paths.cpp
+++ src/bench/shortest_paths.cpp
@@ -20,7 +20,6 @@
 #include <ctime>
 #include <future>
 #include <getopt.h>
-#include <hwloc.h>
 #include <random>
 #include <thread>
 #include <unistd.h>
@@ -33,7 +32,7 @@
 #include "skip_list/skip_queue.h"
 #include "util.h"
 
-constexpr int DEFAULT_NNODES    = 1 << 15;
+constexpr int DEFAULT_NNODES    = 1 << 10;
 constexpr int DEFAULT_NTHREADS  = 1;
 constexpr double DEFAULT_EDGE_P = 0.5;
 constexpr int DEFAULT_SEED      = 0;
@@ -45,6 +44,11 @@ constexpr int DEFAULT_SEED      = 0;
 #define PQ_SEQUENCE   "sequence"
 #define PQ_SKIP       "skip"
 
+static hwloc_wrapper hwloc; /**< Thread pinning functionality. */
+
+static std::atomic<bool> start_barrier(false);
+static std::atomic<int> num_tasks(0);
+
 struct settings {
     int num_nodes;
     int num_threads;
@@ -53,7 +57,30 @@ struct settings {
     std::string type;
 };
 
-static hwloc_topology_t topology;
+struct edge_t {
+    size_t target;
+    size_t weight;
+};
+
+struct vertex_t {
+    edge_t *edges;
+    size_t num_edges;
+    std::atomic<size_t> distance;
+};
+
+struct task_t {
+    task_t(vertex_t *v) : v(v)
+    {
+        num_tasks.fetch_add(1, std::memory_order_relaxed);
+    }
+
+    ~task_t()
+    {
+        num_tasks.fetch_sub(1, std::memory_order_relaxed);
+    }
+
+    vertex_t *v;
+};
 
 static void
 usage()
@@ -75,13 +102,165 @@ usage()
     exit(EXIT_FAILURE);
 }
 
+static vertex_t *
+generate_graph(const size_t n,
+               const int seed,
+               const double p)
+{
+    vertex_t *data = new vertex_t[n];
+
+    std::mt19937 rng;
+    rng.seed(seed);
+    std::uniform_real_distribution<float> rnd_f(0.0, 1.0);
+    std::uniform_int_distribution<size_t> rnd_st(1, std::numeric_limits<int>::max());
+
+    std::vector<edge_t> *edges = new std::vector<edge_t>[n];
+    for (size_t i = 0; i < n; ++i) {
+        for (size_t j = i + 1; j < n; ++j) {
+            if (rnd_f(rng) < p) {
+                edge_t e;
+                e.target = j;
+                e.weight = rnd_st(rng);
+                edges[i].push_back(e);
+                e.target = i;
+                edges[j].push_back(e);
+            }
+        }
+        data[i].num_edges = edges[i].size();
+        if (edges[i].size() > 0) {
+            data[i].edges = new edge_t[edges[i].size()];
+            for (size_t j = 0; j < edges[i].size(); ++j) {
+                data[i].edges[j] = edges[i][j];
+            }
+        } else {
+            data[i].edges = NULL;
+        }
+        data[i].distance = std::numeric_limits<size_t>::max();
+    }
+
+    data[0].distance = 0;
+    delete[] edges;
+
+    return data;
+}
+
+static void
+verify_graph(const vertex_t *graph,
+             const size_t n)
+{
+    for (size_t i = 0; i < n; i++) {
+        const vertex_t *v = &graph[i];
+        const size_t v_dist = v->distance.load(std::memory_order_relaxed);
+
+        for (size_t j = 0; j < v->num_edges; j++) {
+            const edge_t *e = &v->edges[j];
+            const size_t new_dist = v_dist + e->weight;
+
+            const vertex_t *w = &graph[e->target];
+            const size_t w_dist = w->distance.load(std::memory_order_relaxed);
+
+            assert(new_dist >= w_dist);
+        }
+    }
+}
+
+static void
+delete_graph(vertex_t *data,
+             const size_t n)
+{
+    for (size_t i = 0; i < n; ++i) {
+        if (data[i].edges != NULL) {
+            delete[] data[i].edges;
+        }
+    }
+    delete[] data;
+}
+
+template <class T>
+static void
+bench_thread(T *pq,
+             const int thread_id,
+             vertex_t *graph)
+{
+    hwloc.pin_to_core(thread_id);
+
+    while (!start_barrier.load(std::memory_order_relaxed)) {
+        /* Wait. */
+    }
+
+    while (num_tasks.load(std::memory_order_relaxed) > 0) {
+        task_t *task;
+        if (!pq->delete_min(task)) {
+            continue;
+        }
+
+        const vertex_t *v = task->v;
+        const size_t v_dist = v->distance.load(std::memory_order_relaxed);
+        for (size_t i = 0; i < v->num_edges; i++) {
+            const edge_t *e = &v->edges[i];
+            const size_t new_dist = v_dist + e->weight;
+
+            vertex_t *w = &graph[e->target];
+            size_t w_dist = w->distance.load(std::memory_order_relaxed);
+
+            if (new_dist >= w_dist) {
+                continue;
+            }
+
+            bool dist_updated;
+            do {
+                dist_updated = w->distance.compare_exchange_strong(w_dist, new_dist,
+                               std::memory_order_relaxed);
+            } while (!dist_updated && w_dist > new_dist);
+
+            if (dist_updated) {
+                pq->insert(new_dist, new task_t(w));
+            }
+        }
+
+        delete task;
+    }
+}
+
 template <class T>
 static int
 bench(T *pq,
       const struct settings &settings)
 {
     int ret = 0;
+    vertex_t *graph = generate_graph(settings.num_nodes, settings.seed, settings.edge_probability);
 
+    /* Our initial node is graph[0]. */
+
+    pq->insert(0, new task_t(&graph[0]));
+
+    /* Start all threads. */
+
+    std::vector<std::thread> threads;
+    for (int i = 0; i < settings.num_threads; i++) {
+        threads.push_back(std::thread(bench_thread<T>, pq, i, graph));
+    }
+
+    /* Begin benchmark. */
+    start_barrier.store(true, std::memory_order_relaxed);
+
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
+    for (auto &thread : threads) {
+        thread.join();
+    }
+
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    /* End benchmark. */
+
+    assert(num_tasks.load(std::memory_order_relaxed) == 0);
+    verify_graph(graph, settings.num_nodes);
+
+    const double elapsed = timediff_in_s(start, end);
+    fprintf(stdout, "%f\n", elapsed);
+
+    delete_graph(graph, settings.num_nodes);
     return ret;
 }
 
@@ -146,33 +325,28 @@ main(int argc,
 
     s.type = argv[optind];
 
-    hwloc_topology_init(&topology);
-    hwloc_topology_load(topology);
-
     if (s.type == PQ_CLSM) {
-        kpq::clsm<uint32_t, uint32_t> pq;
+        kpq::clsm<uint32_t, task_t *> pq;
         ret = bench(&pq, s);
     } else if (s.type == PQ_GLOBALLOCK) {
-        kpq::GlobalLock pq;
+        kpq::GlobalLock<uint32_t, task_t *> pq;
         ret = bench(&pq, s);
-    } else if (s.type == PQ_LINDEN) {
-        kpq::Linden pq(kpq::Linden::DEFAULT_OFFSET);
-        pq.insert(42); /* A hack to avoid segfault on destructor in empty linden queue. */
-        ret = bench(&pq, s);
-    } else if (s.type == PQ_LSM) {
-        kpq::LSM<uint32_t> pq;
-        ret = bench(&pq, s);
-    } else if (s.type == PQ_SEQUENCE) {
-        kpq::sequence_heap<uint32_t> pq;
-        ret = bench(&pq, s);
-    } else if (s.type == PQ_SKIP) {
-        kpq::skip_queue<uint32_t> pq;
-        ret = bench(&pq, s);
+//    } else if (s.type == PQ_LINDEN) {
+//        kpq::Linden pq(kpq::Linden::DEFAULT_OFFSET);
+//        pq.insert(42); /* A hack to avoid segfault on destructor in empty linden queue. */
+//        ret = bench(&pq, s);
+//    } else if (s.type == PQ_LSM) {
+//        kpq::LSM<uint32_t> pq;
+//        ret = bench(&pq, s);
+//    } else if (s.type == PQ_SEQUENCE) {
+//        kpq::sequence_heap<uint32_t> pq;
+//        ret = bench(&pq, s);
+//    } else if (s.type == PQ_SKIP) {
+//        kpq::skip_queue<uint32_t> pq;
+//        ret = bench(&pq, s);
     } else {
         usage();
     }
-
-    hwloc_topology_destroy(topology);
 
     return ret;
 }
