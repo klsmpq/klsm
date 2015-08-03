6dce706a646980cc1e9741751dd0b604c067e049
Jakob Gruber <jakob.gruber@gmail.com>
2015-08-03 12:15:44 +0200

    Extend and fix versioned array pointer test

--- test/shared_lsm/versioned_array_ptr.cpp
+++ test/shared_lsm/versioned_array_ptr.cpp
@@ -30,33 +30,47 @@ using namespace kpq;
 #define MASK (ARRAY_ALIGNMENT - 1)
 #define NTHREADS (48)
 
+typedef versioned_array_ptr<uint32_t, uint32_t, ARRAY_ALIGNMENT> vap;
+typedef aligned_block_array<uint32_t, uint32_t, ARRAY_ALIGNMENT> aba;
+
 TEST(VersionedArrayPtrTest, SanityCheck)
 {
-    versioned_array_ptr<uint32_t, uint32_t, ARRAY_ALIGNMENT> ptr;
+    vap ptr;
 }
 
 TEST(VersionedArrayPtrTest, Sequential)
 {
-    versioned_array_ptr<uint32_t, uint32_t, ARRAY_ALIGNMENT> ptr;
+    vap ptr;
 
     auto old_ptr = ptr.load_packed();
+    auto old_version = ptr.load()->version();
     ASSERT_NE(nullptr, old_ptr);
-    ASSERT_EQ(0, ((intptr_t)old_ptr) & MASK);
+    ASSERT_EQ(0, ptr.load()->version());
+    ASSERT_EQ(old_version & MASK, ((intptr_t)old_ptr) & MASK);
+    ASSERT_TRUE(vap::matches(old_ptr, old_version));
 
-    aligned_block_array<uint32_t, uint32_t, ARRAY_ALIGNMENT> new_array;
+    aba new_array;
+    new_array.ptr()->increment_version();
+    auto new_version = new_array.ptr()->version();
+    ASSERT_EQ(1, new_version);
+
     ASSERT_TRUE(ptr.compare_exchange_strong(old_ptr, new_array));
+    auto new_ptr = ptr.load_packed();
 
     ASSERT_EQ(new_array.ptr(), ptr.load());
-    ASSERT_EQ(new_array.ptr()->version(), ((intptr_t)ptr.load_packed()) & MASK);
+    ASSERT_EQ(new_array.ptr()->version(), ((intptr_t)new_ptr) & MASK);
+    ASSERT_FALSE(vap::matches(new_ptr, old_version));
+    ASSERT_TRUE(vap::matches(new_ptr, new_version));
 }
 
 static void
 compare_exchange_local(std::atomic<bool> *can_continue,
-                       versioned_array_ptr<uint32_t, uint32_t, ARRAY_ALIGNMENT> *ptr)
+                       std::atomic<int> *num_done,
+                       vap *ptr)
 {
     constexpr int N = 1024;
 
-    aligned_block_array<uint32_t, uint32_t, ARRAY_ALIGNMENT> new_array;
+    aba new_array;
     block_array<uint32_t, uint32_t> *old_array;
 
     while (!can_continue->load()) { }
@@ -68,17 +82,26 @@ compare_exchange_local(std::atomic<bool> *can_continue,
             new_array.ptr()->increment_version();
         } while (!ptr->compare_exchange_strong(old_array, new_array));
     }
+
+    /* Ensure memory the thread-local array isn't being freed prematurely. */
+
+    num_done->fetch_add(1);
+    while (num_done->load() != NTHREADS) { }
 }
 
 TEST(VersionedArrayPtrTest, ParallelReuse)
 {
-    versioned_array_ptr<uint32_t, uint32_t, ARRAY_ALIGNMENT> ptr;
+    vap ptr;
 
     std::vector<std::thread> threads;
     std::atomic<bool> can_continue(false);
+    std::atomic<int> num_done(0);
 
     for (int i = 0; i < NTHREADS; i++) {
-        threads.push_back(std::thread(compare_exchange_local, &can_continue, &ptr));
+        threads.push_back(std::thread(compare_exchange_local,
+                                      &can_continue,
+                                      &num_done,
+                                      &ptr));
     }
 
     can_continue.store(true, std::memory_order_relaxed);
