59acab0ec803f73039522978e9f090ff95a0bfe9
Jakob Gruber <jakob.gruber@gmail.com>
2015-08-07 12:38:41 +0200

    Notes on k-lsm implementation

--- src/k_lsm/k_lsm_inl.h
+++ src/k_lsm/k_lsm_inl.h
@@ -34,6 +34,49 @@ void
 k_lsm<K, V, Rlx>::insert(const K &key,
                          const V &val)
 {
+    /* Insert into the distributed lsm; if the largest block is large enough
+     * (i.e. the next-largest block size would exceed the relaxation bounds),
+     * insert it into the shared lsm instead.
+     *
+     * The concept seems simple, but let's see how easy it will be to integrate
+     * the different memory management schemes.
+     *
+     * ---
+     *
+     * A closer look at dist lsm's insert(): It looks like we can reuse the original
+     * behavior up to the point when merges have been fully performed. Then, if the
+     * block is large enough, 1) insert it into the shared lsm and 2) clear the dist
+     * lsm.
+     *
+     * Items might be read twice (once from dist, once from shared), but that does
+     * not matter since item removal is atomic.
+     *
+     * The biggest issue might be how to integrate memory management of dist lsm
+     * and shared lsm blocks, since they use completely different mechanics: the dist
+     * lsm has a pool relying simply on a 'used' flag, while the shared lsm has
+     * a pool relying on the published version (of the global array) and a status
+     * of each block. Shared blocks (blocks managed by the shared lsm) will never be
+     * seen by the dist lsm, and do not need special consideration. Dist blocks on
+     * the other hand will be seen by shared.
+     *
+     * We could either:
+     * 1)  Copy the contents of the dist block to a shared block upon insertion.
+     *     This adds amortized constant complexity since it's done exactly once
+     *     for each item. It would keep the code simple since we don't need special
+     *     cases for handling dist blocks in shared.
+     * 1a) Instead of copying upon insertion, once we realize the next block will
+     *     exceed our bounds, we could ask the shared lsm to allocate a block for our
+     *     use instead.
+     * 2)  Pass dist blocks into shared. We'd then have ensure they are properly
+     *     marked unused once they are removed, and reserve enough space in the
+     *     dist block pool to handle the additional blocks floating around.
+     *     A problem again is that we cannot guarantee bounds on the number of used
+     *     blocks if we set them unused after successfully updating the global array,
+     *     since the current thread may stall.
+     *
+     * It seems best to start with option 1), optimizing to 1a) in the future.
+     */
+
     m_dist.insert(key, val);
 }
 
@@ -41,5 +84,18 @@ template <class K, class V, int Rlx>
 bool
 k_lsm<K, V, Rlx>::delete_min(V &val)
 {
+    /* Load the best item from the local distributed lsm, and the (relaxed)
+     * best item from the global lsm, and return the best of both.
+     * If both are empty, perform spy on the distributed lsm and retry in
+     * case it succeeds.
+     *
+     * ---
+     *
+     * The changes required for this seem fairly simple: we need to add
+     * peek() to the public interface for both dist and shared lsm's, and
+     * additionally spy() for the dist lsm. There are no further problematic
+     * interactions between memory management here.
+     */
+
     return m_dist.delete_min(val);
 }
