64e22c5b4d5946f2a8ab285a13d7dc3e99a9310e
Jakob Gruber <jakob.gruber@gmail.com>
2015-09-22 17:41:34 +0200

    Remove all empty blocks
    Do not require their capacity to be 1.

--- src/dist_lsm/dist_lsm_local_inl.h
+++ src/dist_lsm/dist_lsm_local_inl.h
@@ -189,7 +189,7 @@ dist_lsm_local<K, V, Rlx>::peek(typename block<K, V>::peek_t &best)
         while (i->size() <= i->capacity() / 2) {
 
             /* Simply remove empty blocks. */
-            if (i->capacity() == 1) {
+            if (i->size() == 0) {
                 const auto next = i->m_next.load(std::memory_order_relaxed);
                 if (i == m_tail) {
                     m_tail = i->m_prev;
